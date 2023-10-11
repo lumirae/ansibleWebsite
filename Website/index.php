9c3afb4e1e79a2fbd449ed2ea1051353b67a6060
KumaBerri <100048633+KumaBerri@users.noreply.github.com>
2023-10-11 13:07:51 +0200

    Add files via upload

---
+++ Website/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="nl-NL">
+
+<head class="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <title>Linda's Design</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <!--Icon page-->
+  <link href="images\weblogo.png" rel="shortcut icon" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+  <img class="phoneimg" src="images\logo.png" width="108">
+  <nav class="nav">
+    <div class="main_list" id="mainListDiv">
+      <ul>
+        <a href="index.php"><u>home</u></a>
+        <a href="portfolio.php">Portfolio</a>
+        <img class="pcimg" src="images\logo.png" width="108">
+        <a href="overmij.php">Over Mij</a>
+        <a href="contact.php">Contact</a>
+      </ul>
+    </div>
+  </nav>
+  <br>
+  <br>
+  <br>
+  <div class="section cc-store-home-wrap">
+    <div class="intro-header">
+      <div class="intro-content cc-homepage">
+        <div class="intro-text">
+          <div class="heading-jumbo">
+            <img src="images/background.png" width="100%" height="auto">
+          </div>
+          <div class="paragraph-bigger cc-bigger-white-light">
+            <br>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br><br><br>
+  <footer>
+    <div class="footer">
+      <a href="https://www.instagram.com/lindasdesign79/" target="_blank"><img src="images/instagram.png" width="38px"></a>
+      <a href="https://www.facebook.com/Lindasdesign79/" target="_blank"><img src="images/facebook.png" width="38px"></a>
+  </footer>
+</body>
+
+</html>
