cffc3cca3d2992f67cfd1a2a19c383da9675f683
KumaBerri <100048633+KumaBerri@users.noreply.github.com>
2023-10-11 13:08:59 +0200

    Add files via upload

---
+++ overmij.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="nl-NL">
+
+<head class="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Linda's Design</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <!--Icon page-->
+    <link href="images\weblogo.png" rel="shortcut icon" type="image/x-icon">
+</head>
+
+<body>
+    <img class="phoneimg" src="images\logo.png" width="108">
+    <nav class="nav">
+        <div class="main_list" id="mainListDiv">
+            <ul>
+                <a href="index.php">home</a>
+                <a href="portfolio.php">Portfolio</a>
+                <img class="pcimg" src="images\logo.png" width="108">
+                <a href="overmij.php"><u>Over Mij</u></a>
+                <a href="contact.php">Contact</a>
+            </ul>
+        </div>
+    </nav>
+    <br>
+    <br><br>
+    <div class="menu">
+        <img class="fotojezelf" src="images/1.jpeg" width="100%" height="auto">
+    </div>
+    <div class="main">
+        <div class="infojezelf">
+            <h2><b>Mijzelf:</b></h2>
+            <p>
+                Schrijf hier over jezelf
+            </p>
+        </div>
+    </div>
+    <br><br><br>
+    <footer>
+        <div class="footer">
+            <a href="https://www.instagram.com/lindasdesign79/" target="_blank"><img src="images/instagram.png" width="38px"></a>
+            <a href="https://www.facebook.com/Lindasdesign79/" target="_blank"><img src="images/facebook.png" width="38px"></a>
+    </footer>
+</body>
+
+</html>
